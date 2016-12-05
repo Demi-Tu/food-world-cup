69f6f7f9f519162ba9291a7d5dd85f4d94a54a09
Anushna Prakash <anushnap@uw.edu>
2016-12-04 16:36:29 -0800

    added code that doesn't work in server.r

--- server.r
+++ server.r
@@ -3,8 +3,17 @@ library(dplyr)
 library(plotly)
 library(shiny)
 
+setwd("~/INFO-201/food-world-cup/")
+source("./scripts/wrangle-data.R")
+food <- read.csv("data/food-world-cup-data.csv")
+
+ratings <- wrangleData(food)
+
+# THIS CODE DOESN'T WORK-- FIX LATER
+ratings.summ <- ratings %>%
+  group_by(country) %>%
+  summarise(mean_rating = mean(rating, na.rm = TRUE))
+
 shinyServer(function(input, output) {
-  setwd("~/INFO-201/food-world-cup/")
-  food <- read.csv("data/food-world-cup-data.csv")
-  
+
 })
