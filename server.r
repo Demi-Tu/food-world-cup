75afc57125bfb92ea8f81683feb926fdc4b22c2d
Anushna Prakash <anushnap@uw.edu>
2016-12-05 18:03:55 -0800

    started gender input, made blank columns NA

--- server.r
+++ server.r
@@ -10,13 +10,25 @@ food <- read.csv("data/food-world-cup-data.csv")
 food$Gender[food$Gender == ""] <- NA
 food <- na.omit(food)
 
+food$Age[food$Age == ""] <- NA
+food$Household.Income[food$Household.Income == ""] <- NA
+food$Education[food$Education == ""] <- NA
+
 ratings <- wrangleData(food)
 
-# THIS CODE DOESN'T WORK-- FIX LATER
+# Create summary table of average ratings
 ratings.summ <- ratings %>%
   group_by(country) %>%
   summarise(mean_rating = mean(rating, na.rm = TRUE))
 
 shinyServer(function(input, output) {
-
+  df <- food
+ 
+   if (input$gender != "Both") {
+    df <- food %>%
+      filter(Gender == input$gender)
+  }
+  
+  # Placeholder line of code, I don't think it works
+  output$value <- renderPrint({input$gender})
 })
--- ui.r
+++ ui.r
@@ -4,6 +4,14 @@ library(shiny)
 
 shinyUI(
   fluidPage(
-    
+    titlePanel('We asked people what their favorite cuisine was... What happened next will shock you!!'),
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons("gender", label = h4("Select gender"),
+                     choices = list("Both" = "Both", "Female" = "Female", "Male" = "Male"), 
+                     selected = 1
+                     )
+      )
+    )
   )
 )
