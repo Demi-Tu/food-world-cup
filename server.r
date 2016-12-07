838be442a13fc9a2d312340e4f97afe6b5365ba2
Anushna Prakash <anushnap@uw.edu>
2016-12-07 10:50:57 -0800

    tweaked map output

--- server.r
+++ server.r
@@ -9,16 +9,20 @@ source("./scripts/wrangle-data.R")
 food <- read.csv("data/food-world-cup-data.csv")
 food <- makePretty(food)
 ratings <- read.csv("data/food-world-cup-combined-cols.csv")
+ratings.summ <- ratings %>%
+  group_by(country, code, Gender, Age, Household.Income) %>%
+  summarize(mean_rating = mean(rating, na.rm = TRUE))
 
 shinyServer(function(input, output) {
+  df <- ratings.summ
   
   # Creates world chloropleth map
   output$world.map <- renderPlotly({
 
     # Show only selected age groups and income brackets
-    df <- ratings.summ %>% filter(Age %in% input$age,
-                        Gender %in% input$gender,
-                        Household.Income %in% input$income)
+    df <- ratings.summ %>% filter(Gender %in% input$gender,
+                                  Age %in% input$age,
+                                  Household.Income %in% input$income)
     
     # light grey boundaries
     l <- list(color = toRGB("grey"), width = 0.5)
